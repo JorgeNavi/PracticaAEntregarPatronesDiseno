2a42677f2b409599276327b2e9bf21bd92da924c
Jorge Navidad <jorge.espliego@outlook.com>
2025-03-19 11:05:24 +0100

    Ajustado el builder para que reciba todos sus parametros

--- AppPatronesNombre/Presentation/HeroDetail/HeroDetailBuilder.swift
+++ AppPatronesNombre/Presentation/HeroDetail/HeroDetailBuilder.swift
@@ -1,7 +1,8 @@
 final class HeroDetailBuilder {
     
     static func build(heroName: String) -> HeroDetailViewController {
-        let viewModel = HeroDetailViewModel(heroName: heroName)
+        let useCase = HeroDetailUseCase()
+        let viewModel = HeroDetailViewModel(heroName: heroName, useCase: useCase)
         let viewController = HeroDetailViewController(viewModel: viewModel)
         return viewController
     }
